7025419a769a28847cf0834f68d7a86867470548
Ahmed Radeef <ahmed.radeef86@gmail.com>
2022-01-13 22:19:17 -0800

    fix the header

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,9 +6,20 @@ class LandingPage extends Component {
 	
 	render() {
 		return (
-			<>
-				<header>
-					<nav></nav>
+			< div className="LandingPage">
+				<header className="header">
+					<h2 className="header__logo"><img src={logo} alt="logo" /></h2>
+					<nav className="header__list">
+						<li className="header__item ">Game Room</li>
+						<li className="header__item">Evenets</li>
+						<li className="header__item header__item--active">Loaderboard</li>
+						<li className="header__item">Shop</li>
+					</nav>
+					<article className="user-card">
+						<img className="user-card__img" src={userImage} alt="user" />
+						<h2 className="user-card__rank">Current Rank #4</h2>
+						<h2 className="user-card__point">Points 2630</h2>
+					</article>
 				</header>
 				<main>
 					<h1>Game Room</h1>
@@ -22,7 +33,7 @@ class LandingPage extends Component {
 					<h2>1</h2>
 					<img src="" alt="" />
 				</footer>
-			</>
+			</div>
 		);
 	}
 }
