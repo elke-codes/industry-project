ffefa8fbca05e81751bb4277c60d5d3768935004
Elke Dick <elke.codes@gmail.com>
2022-01-13 16:59:22 -0800

    landingpage skeleton

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./LandingPage.scss";
+
+class LandingPage extends Component {
+	render() {
+		return (
+			<>
+				<header>
+					<nav></nav>
+				</header>
+				<main>
+					<h1>Game Room</h1>
+					<p>description Lorem ipsum dolor, sit amet consectetur </p>
+					<SearchBar />
+					{/* games.map---> */}
+					<GameCard />
+				</main>
+				<footer>
+					<img src="" alt="" />
+					<h2>1</h2>
+					<img src="" alt="" />
+				</footer>
+			</>
+		);
+	}
+}
+
+export default LandingPage;
